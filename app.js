93c2b2fdb91868498e77901084478c20b3b14f87
Marius Mukunzi <riamarius@gmail.com>
2015-09-20 21:15:39 -0400

    Added posts/comments page

--- app.js
+++ app.js
@@ -6,20 +6,27 @@ app.config([
     '$stateProvider',
     '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
-    
-    // set up home route
-    $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: '/home.html',
-            controller: 'MainCtrl'
-        });
-    // handle the route if the app receives a URL that is not defined
-    $urlRouterProvider.otherwise('home');
-}]);
+
+        // set up home route
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+
+        // handle the route if the app receives a URL that is not defined
+        $urlRouterProvider.otherwise('home');
+    }
+]);
 
 // posts service
-app.factory('posts', [function(){
+app.factory('posts', [function() {
     var o = {
         posts: []
     };
@@ -28,30 +35,71 @@ app.factory('posts', [function(){
 
 // main controller
 app.controller('MainCtrl', [
-    '$scope', 
+    '$scope',
     'posts',
     function($scope, posts) {
-    
-    // $scope variable
-    $scope.test = 'Hello Marius!';
-    // bind $scope.posts variable to the posts array in the posts service
-    $scope.posts = posts.posts;
 
-    // add new post
-    $scope.addPost = function(){
-    	if (!$scope.title || $scope.title === '') {
-    		return;
-    	};
-    	$scope.posts.push({
-            title: $scope.title,
-            link: $scope.link, 
-            upvotes: 0});
-    	$scope.title = '';
-        $scope.link = '';
-    }
+        // $scope variable
+        $scope.test = 'Hello Marius!';
+        // bind $scope.posts variable to the posts array in the posts service
+        $scope.posts = posts.posts;
 
-    // upvoting an existing post
-    $scope.incrementUpvotes = function(post) {
-    	post.upvotes += 1;
+        // add new post
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '') {
+                return;
+            };
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0,
+                comments: [{
+                    author: 'Joe',
+                    body: 'Cool post!',
+                    upvotes: 0
+                }, {
+                    author: 'Bob',
+                    body: 'Great idea but everything is wrong!',
+                    upvotes: 0
+                }]
+            });
+            $scope.title = '';
+            $scope.link = '';
+        }
+
+        // upvoting an existing post
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        }
     }
-}])
+])
+
+// posts controller
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+
+        // add comment
+        $scope.addComment = function(){
+            if ($scope.body === '') { return; };
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        }
+
+    }
+])
+
+
+
+
+
+
+
+
